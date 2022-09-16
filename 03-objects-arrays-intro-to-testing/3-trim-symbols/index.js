ada0eca385b0f85fe270a697acf505c007cffb30
Smirnykh <v.smirnykh@icode.by>
2022-09-17 01:56:54 +0300

    task 3 fix test

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,11 +5,15 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-    let result = '';
-    let count = 0;
+    if (!size) {
+        return string;
+    }
     if (size === 0){
         return '';
     }
+    let result = '';
+    let count = 0;
+
     string.split('').forEach(symbol => {
         if (result.at(-1) === symbol){
             if (size !== count){
