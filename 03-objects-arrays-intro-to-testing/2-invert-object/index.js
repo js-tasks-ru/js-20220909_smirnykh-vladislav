e431306f1e4457c4453555a0672bad4c6fe627d0
Smirnykh <v.smirnykh@icode.by>
2022-09-17 01:51:01 +0300

    task 3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
-}
+    if (!obj){
+        return undefined
+    };
+    const newObj = {}
+    for (let [key, value] of Object.entries(obj)) {
+        if (key && value){
+            newObj[value] = key
+        };
+    };
+    return newObj
+};
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
-}
+    let result = '';
+    let count = 0;
+    if (size === 0){
+        return '';
+    }
+    string.split('').forEach(symbol => {
+        if (result.at(-1) === symbol){
+            if (size !== count){
+                count++;
+                result += symbol;
+            };
+        } else {
+            count = 1;
+            result += symbol;
+        };
+    });
+    return result
+};
