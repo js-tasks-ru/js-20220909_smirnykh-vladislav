414952d35abe9b46d41560f477da8b132969305c
Smirnykh <v.smirnykh@icode.by>
2022-09-17 02:03:35 +0300

    task 3 fix test

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,7 +4,7 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export function trimSymbols(string, size=false) {
     if (!size) {
         return string;
     }
